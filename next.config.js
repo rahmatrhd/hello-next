6d4a6319fbaafe6def6d703b4d82d5cd8ea1d2cd
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2019-08-01 16:59:29 +0700

    remove typescript dependecies and config for previous next.js version

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
-const path = require('path')
-const withTypescript = require('@zeit/next-typescript')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
-
 const nextConfig = {
   distDir: '../../.next', // relative to `pages` dir, see scripts in package.json
-  webpack(config, options) {
-    if (options.isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin({
-        async: false,
-        formatter: typescriptFormatter,
-        tsconfig: path.resolve('./tsconfig.json'),
-        watch: path.resolve('./src'),
-      }))
-    }
-
-    return config
-  }
 }
 
-module.exports = withTypescript(nextConfig)
+module.exports = nextConfig
